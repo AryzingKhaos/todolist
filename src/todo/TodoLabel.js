4a68a8bc20e6ab069e7886fb78c5e82ece392a4c
hwyComputer-2 <eesorby@163.com>
2019-03-08 13:39:19 +0800

    完成了基本功能

--- src/todo/TodoLabel.js
+++ src/todo/TodoLabel.js
@@ -20,7 +20,7 @@ class TodoLabel extends Component {
 	}
 
 	render() {
-		const {keyValue, title, desc, isComplete, children} = this.props; // 基础信息
+		const {keyValue, title, desc, isComplete, children, isArrange} = this.props; // 基础信息
 		const {searchTodoByKey, setTodoTitle, setTodoDesc, labelAddChild, labelRemoveThis, labelSetCompleteValue} = this.props; // function部分
 		const {marginLeft, perMarginLeft, fontSize, perFontSize} = this.props; // css样式部分
 		// console.log(marginLeft,perMarginLeft, fontSize, perFontSize);
@@ -28,7 +28,7 @@ class TodoLabel extends Component {
 			<div className={"todolabel" + (isComplete ? ' completed' : '')}>
 
 				{/* title部分 */}
-				<div className="title" alt="双击编辑" style={{'marginLeft':marginLeft, 'fontSize': fontSize}}>
+				<div className="title" alt="双击编辑" style={{'marginLeft':marginLeft, 'fontSize': fontSize, 'width':'calc( 80% - '+marginLeft+'px )'}}>
 					<div className="titleText" 
 						onClick={()=> {this.setState({showContent: !this.state.showContent})}} 
 						onDoubleClick = {()=> {this.setState({isEditTitle: !this.state.isEditTitle})}}
@@ -54,10 +54,25 @@ class TodoLabel extends Component {
 							}}
 						>确定</div>
 					</div>
+
+					{/* 完成按钮 */}
+					<div className="complete-this">
+						<input type="checkbox" name="mycheckbox" id={"mycheckbox"+keyValue} checked={(isComplete ? 'checked' : '')}
+							onChange={(e) => {
+								console.log(e.target.checked);
+								if(e.target.checked){
+									labelSetCompleteValue(keyValue, true);
+								}else{
+									labelSetCompleteValue(keyValue, false);
+								}
+							}}
+						/>
+						<label htmlFor={"mycheckbox"+keyValue}><i className="checkbox-dot"></i></label>
+					</div>
 				</div>
 
 				{/* desc部分 */}
-				<div className={"desc"+(this.state.showContent ? ' show' :'')} style={{'marginLeft':marginLeft}}>
+				<div className={"desc"+(this.state.showContent ? ' show' :'')} style={{'marginLeft':marginLeft,'width':'calc( 80% - '+marginLeft+'px )'}}>
 					<div className="descText" 
 						onClick={()=> {this.setState({showContent: !this.state.showContent})}} 
 						onDoubleClick = {()=> {this.setState({isEditDesc: !this.state.isEditDesc})}}
@@ -90,26 +105,18 @@ class TodoLabel extends Component {
 
 				<div className="remove-this" onClick={() => {labelRemoveThis(keyValue)}}>-</div>
 
-				<div className="complete-this">
-					<input type="checkbox" name="mycheckbox" id={"mycheckbox"+keyValue} checked={(isComplete ? 'checked' : '')}
-						onChange={(e) => {
-							console.log(e.target.checked);
-							if(e.target.checked){
-								labelSetCompleteValue(keyValue, true);
-							}else{
-								labelSetCompleteValue(keyValue, false);
-							}
-						}}
-					/>
-            		<label htmlFor={"mycheckbox"+keyValue}><i className="checkbox-dot"></i></label>
-				</div>
-
 				<div className="children">
 					{
 						function() {
 							// console.log(children && children.length);
 							if(children && children.length){
-								return children.map((item, index) => {
+								let copyChildren = children.concat([]);
+								if(isArrange){
+									copyChildren.sort(function(a,b){
+										return a.isComplete - b.isComplete;
+									})
+								}
+								return copyChildren.map((item, index) => {
 									return (
 										<TodoLabel 
 											key={item.key}
@@ -118,6 +125,7 @@ class TodoLabel extends Component {
 											desc={item.desc}
 											isComplete={item.isComplete}
 											children={item.children}
+											isArrange={isArrange}
 
 											searchTodoByKey = {searchTodoByKey}
 											setTodoTitle = {setTodoTitle}
--- src/todo/TodoList.js
+++ src/todo/TodoList.js
@@ -9,34 +9,40 @@ class TodoLsit extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			isArrange: false, // 是否整理
 			todo: [{
 				key: 1548577424000,
 				parentKey: null,
 				isComplete: false,
+				completedTime: 1548577424000,
 				title:'todo1',
 				desc: 'todo desc1',
 				children:[{
 					key: 1548577424010,
 					parentKey: 1548577424000,
 					isComplete: false,
+					completedTime: 1548577424000,
 					title:'todo1-child1',
 					desc: 'todo1-child1 desc1',
 					children:[{
 						key: 1548577424100,
 						parentKey: 1548577424010,
 						isComplete: false,
+						completedTime: 1548577424000,
 						title:'todo1-child1-child1',
 						desc: 'todo1-child1-child1 desc1',
 						children:[{
 							key: 1548577425000,
 							parentKey: 1548577424100,
 							isComplete: false,
+							completedTime: 1548577424000,
 							title:'todo1-child1-child1-child1',
 							desc: 'todo1-child1-child1-child1 desc1',
 							children:[{
 								key: 1548577435000,
 								parentKey: 1548577425000,
 								isComplete: false,
+								completedTime: 1548577424000,
 								title:'todo1-child1-child1-child1-child1',
 								desc: 'todo1-child1-child1-child1-child1 desc1',
 								children:[],
@@ -48,6 +54,7 @@ class TodoLsit extends Component {
 				key: 1548577424001,
 				parentKey: null,
 				isComplete: false,
+				completedTime: 1548577424000,
 				title:'todo2',
 				desc: 'todo desc2',
 				children:[],
@@ -55,6 +62,7 @@ class TodoLsit extends Component {
 				key: 1548577424002,
 				parentKey: null,
 				isComplete: false,
+				completedTime: 1548577424000,
 				title:'todo3',
 				desc: 'todo desc3',
 				children:[],
@@ -62,13 +70,16 @@ class TodoLsit extends Component {
 				key: 1548577424003,
 				parentKey: null,
 				isComplete: false,
+				completedTime: 1548577424000,
 				title:'todo3',
 				desc: 'todo desc3',
 				children:[],
 			}],
 		};
 		this.loadTodo = this.loadTodo.bind(this);
+		this.newTodo = this.newTodo.bind(this);
 		this.saveTodo = this.saveTodo.bind(this);
+		this.arrangeTodo = this.arrangeTodo.bind(this);
 
 		this.searchTodoByKey = this.searchTodoByKey.bind(this);
 		this.setTodoTitle = this.setTodoTitle.bind(this);
@@ -77,11 +88,14 @@ class TodoLsit extends Component {
 		this.labelRemoveThis = this.labelRemoveThis.bind(this);
 		this.labelSetCompleteValue = this.labelSetCompleteValue.bind(this);
 		this.checkChildrenIsAllCompleted = this.checkChildrenIsAllCompleted.bind(this);
+		this.labelSetParentsCompletedValue = this.labelSetParentsCompletedValue.bind(this);
+		this.labelSetChildrenCompletedValue = this.labelSetChildrenCompletedValue.bind(this);
 		
 	}
 
 	componentDidMount() {
 		this.loadTodo();
+		// this.setState({isArrange: true});
 	}
 
 	loadTodo(){
@@ -91,6 +105,21 @@ class TodoLsit extends Component {
 		})
 	}
 
+	newTodo(){
+		let newKey = Date.parse(new Date())+parseInt(Math.random() * 1000);
+		console.log(newKey);
+		let parentKey = null;
+		this.state.todo.push({
+			key: newKey,
+			parentKey: parentKey,
+			isComplete: false,
+			title:'新建todo',
+			desc: '新建todo desc',
+			children:[],
+		})
+		this.setState({todo: this.state.todo});
+	}
+
 	saveTodo(){
 		let {todo} = this.state;
 		API.saveTodo(todo).then(data => {
@@ -98,6 +127,10 @@ class TodoLsit extends Component {
 			if(data.code === 0)
 				alert('保存成功~');
 		})
+	}
+
+	arrangeTodo(){
+		this.setState({isArrange: !this.state.isArrange});
 	}
 
 	searchTodoByKey(key, todo){
@@ -163,25 +196,51 @@ class TodoLsit extends Component {
 	}
 
 	labelSetCompleteValue(key, bool){
+		// console.log('执行labelSetCompleteValue'+key);
 		let todo = this.searchTodoByKey(key);
-		if(bool === true){// bool只允许是true和false
-			todo.isComplete = true;
-		}else{
-			todo.isComplete = false;
-		}
+		if(!todo) return;
+		bool === true ? (todo.isComplete = true) :  (todo.isComplete = false);
 		
-		if(this.checkChildrenIsAllCompleted(todo.parentKey)){ // 如果这个元素的父对象也查询到子对象全部完成了
-			this.labelSetCompleteValue(todo.parentKey, true); // 递归调用，设定父对象也为“已完成”
-		}
+		// 这里做双向延伸，如果当前的todo完成了，那么去设置父todo完成
+		// 由labelSetParentsCompleteValue判断当前的todo的父todo是否完成
+		// 同时，向子todo延伸，由labelSetChildrenCompletedValue判断当前todo的子todo是否完成
+		// 如果完成，那么向子todo延伸
+		this.labelSetParentsCompletedValue(key, bool);
+		this.labelSetChildrenCompletedValue(todo.children, bool);
+
 		this.setState({todo: this.state.todo});
 	}
 
-	checkChildrenIsAllCompleted(parentKey){
-		let parentTodo = this.searchTodoByKey(parentKey);
-		if(!parentTodo) return false;
-		for(let i = 0; i < parentTodo.children.length; i++ ){
-			if(!parentTodo.children[i].isComplete) return false;
+	// 设置父todo的是否完成值，并且只向父todo延伸
+	labelSetParentsCompletedValue(key, bool){
+		let todo = this.searchTodoByKey(key);
+		if(!todo) return;
+		// console.log(todo.title);
+		bool === true ? (todo.isComplete = true) :  (todo.isComplete = false);
+		if(bool === true && this.checkChildrenIsAllCompleted(todo.parentKey)){ // 如果这个元素的父对象也查询到子对象全部完成了
+			this.labelSetParentsCompletedValue(todo.parentKey, true); // 递归调用，设定父对象也为“已完成”
+		}else if(bool === false){ // 子对象未完成，那么所有上级都是未完成
+			this.labelSetParentsCompletedValue(todo.parentKey, false);
 		}
+	}
+
+	// 设置子todo的是否完成值，并且只向子todo延伸
+	labelSetChildrenCompletedValue(children, bool){
+		if(!children.length) return;
+		for(let i = 0; i < children.length; i++){
+			bool === true ? (children[i].isComplete = true) :  (children[i].isComplete = false);
+			this.labelSetChildrenCompletedValue(children[i].children, bool); // 递归调用，设定子对象也为“已完成”
+		}
+	}
+
+	// 判断当前todo的子对象是否都已经完成了
+	checkChildrenIsAllCompleted(key){
+		let todo = this.searchTodoByKey(key);
+		if(!todo) return false;
+		for(let i = 0; i < todo.children.length; i++ ){
+			if(!todo.children[i].isComplete) return false;
+		}
+		// console.log(todo.title+' is completed!');
 		return true;
 	}
 
@@ -189,15 +248,27 @@ class TodoLsit extends Component {
 
 	render() {
 		console.log(this.state.todo);
+		let copyTodo = this.state.todo.concat([]);
+		if(this.state.isArrange){
+			copyTodo.sort(function(a, b){
+				return a.isComplete - b.isComplete;
+			})
+		}
 		return (
 			<div className="main">
 				<div className="todoLabelWrap">
-					{this.state.todo.map((item, index)=>{
+					{copyTodo.map((item, index)=>{
 						// console.log(item);
 						return (
 							<TodoLabel 
+								// 主要数据
 								key={item.key}
 								keyValue={item.key}
+								isComplete={item.isComplete}
+								children={item.children}
+								isArrange={this.state.isArrange}
+
+								// 方法
 								searchTodoByKey = {this.searchTodoByKey}
 								setTodoTitle = {this.setTodoTitle}
 								setTodoDesc = {this.setTodoDesc}
@@ -205,19 +276,24 @@ class TodoLsit extends Component {
 								labelRemoveThis = {this.labelRemoveThis}
 								labelSetCompleteValue = {this.labelSetCompleteValue}
 
+								// 文本
 								title={item.title}
 								desc={item.desc}
-								isComplete={item.isComplete}
-								children={item.children}
+
+								// 样式
 								marginLeft={0}
 								perMarginLeft={40}
-								fontSize={32}
-								perFontSize={4}
+								fontSize={16}
+								perFontSize={0}
 							/>
 						);
 					})}
 				</div>
-				<div className="divact save-btn" onClick={() => {this.saveTodo()}}>保存</div>
+				<div className="btnbox">
+					<div className="divact btn new-btn" onClick={() => {this.newTodo()}}>新增Todo</div>
+					<div className="divact btn arrange-btn" onClick={() => {this.arrangeTodo()}}>{this.state.isArrange ? '不整理' : '整理'}</div>
+					<div className="divact btn save-btn" onClick={() => {this.saveTodo()}}>保存</div>
+				</div>
 			</div>
 		);
 	}
